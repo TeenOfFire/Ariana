e0c9e4a582e7c8c2e1307d37288e3ffad11e1b78
Teen Fire <102863642+TeenOfFire@users.noreply.github.com>
2023-02-09 13:52:41 +0530

    Update info.py

--- info.py
+++ info.py
@@ -13,7 +13,7 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ.get('API_ID', '1736204'))
 API_HASH = environ.get('API_HASH', '890d40e0f91a4de32dec2965444b2cbe')
-BOT_TOKEN = environ.get('BOT_TOKEN', '6062946689:AAFg3rWb9CxWOL83EkYYFn07okosmQMA7S4')
+BOT_TOKEN = environ.get('BOT_TOKEN', '')
 
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
